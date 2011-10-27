a0773696256b3fbc77d055d60711db9190099aae
Steven Shaw <steven@steshaw.org>
2011-10-27 22:18:50 +1000

    Name the QuickCheck properties for the quickCheck script

--- Part4.hs
+++ Part4.hs
@@ -10,16 +10,16 @@ runLengthEncode (x:xs) = nextGroup x 1 xs
       | otherwise = (e, n) : nextGroup y  1      ys
 
 
-rlePropLengthPreserved :: [Int] -> Bool
-rlePropLengthPreserved as = length as == (sum $ map snd $ runLengthEncode as)
+prop_rleLengthPreserved :: [Int] -> Bool
+prop_rleLengthPreserved as = length as == (sum $ map snd $ runLengthEncode as)
 
-rlePropDupesCollapsed :: Int -> Bool
-rlePropDupesCollapsed n
+prop_rleDupesCollapsed :: Int -> Bool
+prop_rleDupesCollapsed n
   | m == 0    = runLengthEncode "" == []
   | otherwise = runLengthEncode (replicate m 'x') == [('x', m)]
   where m = n `mod` 100
 
-rlePropRoundTrip :: [Int] -> Bool
-rlePropRoundTrip ns = runLengthEncode xs == is
+prop_rleRoundTrip :: [Int] -> Bool
+prop_rleRoundTrip ns = runLengthEncode xs == is
   where is = zip ['a'..] $ map (\n -> n `mod` 100 + 1) ns
         xs = concatMap (\(i,n) -> replicate n i) is
